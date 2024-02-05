9c81bb54f3cfa32798c172bd44c3d00b23d82fa7
323 <323@323-1>
2024-02-05 11:05:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            double x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("y = " + (Math.Sin(x) - Math.Cos(x)) / 5);
+            Console.ReadKey();
+        }
+    }
+}
